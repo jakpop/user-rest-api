48aa1310312672ec96689f721572346722f7507c
Jakub Popiół <popioljakub@gmail.com>
2020-03-20 14:45:33 +0100

    minor changes

--- src/main/java/com/example/userrestapi/repository/UserRepository.java
+++ src/main/java/com/example/userrestapi/repository/UserRepository.java
@@ -6,7 +6,9 @@ import org.springframework.stereotype.Repository;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import java.util.function.Predicate;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import com.example.userrestapi.domain.User;
 
