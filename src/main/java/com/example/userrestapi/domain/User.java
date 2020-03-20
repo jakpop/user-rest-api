1f1d8a1a2adb3bbb7ecf51fb25f55740fba084b2
Jakub Popiół <popioljakub@gmail.com>
2020-03-20 20:27:49 +0100

    added MongoDB

--- src/main/java/com/example/userrestapi/domain/User.java
+++ src/main/java/com/example/userrestapi/domain/User.java
@@ -1,16 +1,20 @@
 package com.example.userrestapi.domain;
 
 import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 
+@Document
 @Data
 public class User {
 
-    private long id;
+    @Id
+    private String id;
     private String name;
     private String surname;
     private String email;
 
-    public User(long id, String name, String surname, String email) {
+    public User(String id, String name, String surname, String email) {
         this.id = id;
         this.name = name;
         this.surname = surname;
